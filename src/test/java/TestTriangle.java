6031770d5c4bbd142ea0e50bae336c9f36b36624
Alina <alinchik-95@mail.ru>
2021-12-10 11:09:56 +0300

    triangle test report

--- src/test/java/TestTriangle.java
+++ src/test/java/TestTriangle.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TestTriangle {
     // одна сторона слишком большая
@@ -9,9 +11,12 @@ public class TestTriangle {
     // могут передать слишком большие значения
     // могут быть переданы отрицательные значение
 
+    private static Logger logger = (Logger) LoggerFactory.getLogger(TestTriangle.class);
+
     @Test
     @DisplayName("одна сторона слишком большая")
     void largeSideTest() {
+        logger.info("Попытка создать треугольник с большой стороной");
         Assertions.assertThrows(Triangle.SideIsTooLargeException.class, () -> {
             new Triangle(8, 8, 228);
         });
@@ -20,6 +25,7 @@ public class TestTriangle {
     @Test
     @DisplayName("как минимум одна из сторон имеет значение 0")
      void zeroSideTest() {
+        logger.info("Попытка создать треугольник с нулевой стороной");
         Assertions.assertThrows(Triangle.NonPositiveSideException.class, () -> {
             new Triangle(8, 0, 228);
         });
@@ -28,6 +34,7 @@ public class TestTriangle {
     @Test
     @DisplayName("как минимум одна из сторон имеет отрицательное")
     void negativeSideTest() {
+        logger.info("Попытка создать треугольник с отрицательной стороной");
         Assertions.assertThrows(Triangle.NonPositiveSideException.class, () -> {
             new Triangle(8, -10, 228);
         });
